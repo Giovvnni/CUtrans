05042e560d54b3beb71b8eaef53c66ea52839bc6
Giovvni <giovanni.vasquez@alumnos.uv.cl>
2024-12-24 13:09:33 -0300

    fix(main): reutilizando

--- jest.config.ts
+++ jest.config.ts
@@ -7,12 +7,22 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    '^@/components/(.*)$': '<rootDir>/components/$1'
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1' // Si usas alias para páginas
   },
   testEnvironment: 'jest-environment-jsdom',
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest'
-  }
+  },
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts']
 }
 
 export default createJestConfig(customJestConfig)
---
+++ src/example/example.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Example: React.FC = () => {
+  return <div>This is an example component.</div>
+}
+
+export default Example
