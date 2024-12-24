5933daae09b5b5591bfbe0cc8af5bde42ca56603
Giovvni <giovanni.vasquez@alumnos.uv.cl>
2024-12-24 11:13:08 -0300

    fix(main): reutilizando

--- src/layouts/regular-layout/RegularLayout.tsx
+++ src/layouts/regular-layout/RegularLayout.tsx
@@ -3,11 +3,8 @@ import React from 'react'
 
 // TODO Arreglar importaciones
 
-import MobileHeader from './components/header/mobile-header/MobileHeader'
-
 import { Footer } from './components/footer'
 import { NavBar } from './components/navbar'
-import { TopBar } from './components/top-bar'
 
 interface RootLayoutProps {
   children: React.ReactNode
@@ -17,12 +14,9 @@ const RegularLayout: React.FC<RootLayoutProps> = ({ children }) => {
   return (
     <>
       <header className="hidden md:block">
-        <TopBar />
         <NavBar />
       </header>
-      <header className="block md:hidden">
-        <MobileHeader />
-      </header>
+      <header className="block md:hidden"></header>
       <main>{children}</main>
       <footer>
         <Footer />
